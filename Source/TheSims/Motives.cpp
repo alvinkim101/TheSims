3d8b4c4b472119b1df150e8416145c892f8407a4
Alvin Kim <alvinkim101@gmail.com>
2024-05-19 22:37:58 -0700

    feat: create Motives ActorComponent

---
+++ Source/TheSims/Motives.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Motives.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values for this component's properties
+UMotives::UMotives()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+}
+
+
+// Called when the game starts
+void UMotives::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+}
+
+
+// Called every frame
+void UMotives::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	for (auto& Motive : Motives)
+	{
+		if (Motive.Value.second == true)
+		{
+			Motive.Value.first = UKismetMathLibrary::FMin(Motive.Value.first + Rate * DeltaTime, Max);
+		}
+		else
+		{
+			Motive.Value.first = UKismetMathLibrary::FMax(Motive.Value.first - Rate * DeltaTime, -Max);
+		}
+	}
+}
+
+FGameplayTag UMotives::ChooseMotive() const
+{
+	if (Motives[Energy].first < 0)
+	{
+		return Energy;
+	}
+	else
+	{
+		return Fun;
+	}
+}
+
+void UMotives::SetRegenState(FGameplayTag Motive, bool RegenState)
+{
+	Motives[Motive].second = RegenState;
+}
---
+++ Source/TheSims/Motives.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameplayTagsManager.h"
+#include "Motives.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class THESIMS_API UMotives : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMotives();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	FGameplayTag ChooseMotive() const;
+
+	UFUNCTION(BlueprintCallable)
+	void SetRegenState(FGameplayTag Motive, bool RegenState);
+
+private:
+	FGameplayTag Fun = UGameplayTagsManager::Get().RequestGameplayTag(TEXT("Motive.Fun"));
+	FGameplayTag Energy = UGameplayTagsManager::Get().RequestGameplayTag(TEXT("Motive.Energy"));
+
+	// Rate at which motive regens / decays
+	UPROPERTY(EditDefaultsOnly)
+	float Rate = 1.f;
+
+	// Max value of motive
+	UPROPERTY(EditDefaultsOnly)
+	float Max = 100.f;
+
+	// Key: motive name, Value: motive value and whether it is regenerating or not
+	TMap<FGameplayTag, std::pair<float, bool>> Motives {
+		{ Fun, { Max, false } },
+		{ Energy, { Max, false } },
+	};
+};
